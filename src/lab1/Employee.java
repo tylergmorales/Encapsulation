df9eedb6ca7a74c24a5a56559e47f888d06b463d
Tyler <Tyler@Tyler>
2017-01-31 22:13:02 -0600

    finished lab2

---
+++ src/lab1/Employee.java
@@ -0,0 +1,107 @@
+package lab1;
+
+import java.util.Date;
+
+/**
+ * Fix the code in this class to do PROPERTY encapsulation correctly. Also
+ * consider if any of the properties should be mandatory and use a 
+ * constructor to enforce that. Review the tips in the document 
+ * "EncapCheckList.pdf" if needed.
+ *
+ * @author      Jim Lombardo, WCTC Instructor
+ * @version     1.02
+ */
+public class Employee {
+    public final String COMPANY_NAME = "Acme";
+    private String firstName;
+    private String lastName;
+    private String ssn;
+    private boolean metWithHr;
+    private boolean metDeptStaff;
+    private boolean reviewedDeptPolicies;
+    private boolean movedIn;
+    private String cubeId;
+    private Date orientationDate;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        if(ssn == null || ssn.length() != 9)
+        {
+            System.out.println("Please enter valid SSN");
+            return;
+        }
+        
+    }
+
+    public boolean isMetWithHr() {
+        return metWithHr;
+    }
+
+    public void setMetWithHr(boolean metWithHr) {
+        this.metWithHr = metWithHr;
+    }
+
+    public boolean isMetDeptStaff() {
+        return metDeptStaff;
+    }
+
+    public void setMetDeptStaff(boolean metDeptStaff) {
+        this.metDeptStaff = metDeptStaff;
+    }
+
+    public boolean isReviewedDeptPolicies() {
+        return reviewedDeptPolicies;
+    }
+
+    public void setReviewedDeptPolicies(boolean reviewedDeptPolicies) {
+        this.reviewedDeptPolicies = reviewedDeptPolicies;
+    }
+
+    public boolean isMovedIn() {
+        return movedIn;
+    }
+
+    public void setMovedIn(boolean movedIn) {
+        this.movedIn = movedIn;
+    }
+
+    public String getCubeId() {
+        return cubeId;
+    }
+
+    public void setCubeId(String cubeId) {
+        this.cubeId = cubeId;
+    }
+
+    public Date getOrientationDate() {
+        return orientationDate;
+    }
+
+    public void setOrientationDate(Date orientationDate) {
+        this.orientationDate = orientationDate;
+    }
+    
+    
+
+
+
+}
